ce4229700214867ecc82fa482d4032a60bce61cb
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-09-14 16:12:08 +0200

    add sass

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import '../sass/layout.scss'
 
 const Layout = ({ children }) => {
   return (
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -6,7 +6,7 @@ export default class tours extends Component {
   render() {
     return (
       <Layout>
-        <p> Dolor ut cillum esse duis sunt aliquip proident est ipsum anim sit incididunt eu. Laboris dolore id exercitation tempor cillum. Sint labore dolor qui velit quis ullamco nulla dolor do excepteur tempor consequat est ea. Exercitation aute qui aute occaecat eiusmod aliquip labore enim incididunt labore nostrud exercitation veniam fugiat. Sit sunt exercitation occaecat tempor aliqua deserunt reprehenderit enim sunt adipisicing cillum sit. Labore occaecat proident voluptate tempor et incididunt aliquip incididunt ullamco. In nostrud mollit anim sunt id Lorem sint nostrud ad.</p>
+        <p>Payant comme une vue sur la mer gratuit comme une vue sur uranus.</p>
       </Layout>
     )
   }
