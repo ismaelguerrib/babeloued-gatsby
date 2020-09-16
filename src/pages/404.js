f36e051e964198b019560a3196c7e5fe025213aa
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-09-16 15:09:40 +0200

    adding styles to custom  404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Banner from '../components/Banner'
+
+import styles from '../css/error.module.css'
+
 
 export default function error() {
   return (
     <Layout>
-      <p> igo tu t'es trompé de chemin</p>
-    </Layout>
+      <header className={styles.error}>
+        <Banner title="Comment t'es arrivé là ?">
+          <Link to="/" className="btn-white">suis moi je te raccompagne</Link>
+        </Banner>
+      </header>
+
+    </Layout >
   )
 }
