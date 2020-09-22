84740495e4555e8dfe6371570bb4ec460c1e4b1d
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-09-22 11:33:34 +0200

    set up home background with gastby optimisation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,35 @@
 import React, { Fragment } from "react"
 import { Link } from "gatsby"
 import Layout from '../components/Layout'
-import SimpleHero from '../components/SimpleHero'
+import StyledHero from '../components/SimpleHero'
 import Banner from "../components/Banner"
 import About from "../components/About"
 import Services from "../components/Services"
+import { graphql } from 'gatsby'
 
-export default () => {
+export default ({ data }) => {
   return (
     <Fragment>
       <Layout>
-        <SimpleHero>
+        <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
           <Banner title="Viens au bled !" info="Ca fait combien de temps que tu n'es pas allé au bled ?">
             <Link to="/tours" className="btn-white">partir maintenant</Link>
           </Banner>
-        </SimpleHero>
+        </StyledHero>
         <About />
         <Services />
       </Layout>
 
     </Fragment>
   )
-
 }
+export const query = graphql`
+query {
+  defaultBcg:file(relativePath:{eq:"defaultBcg.jpeg"}){
+    childImageSharp{
+      fluid(quality:90, maxWidth:4160){
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+}`
